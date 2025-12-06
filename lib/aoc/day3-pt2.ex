f4ce4fc48f0d7729a5586d7654434312226c0cfc
James Potter <jamesrpotter1@gmail.com>
2025-12-06 10:54:12 +0000

    Day 3 part 2

--- lib/aoc/day3-pt2.ex
+++ lib/aoc/day3-pt2.ex
@@ -1,27 +1,24 @@
 defmodule Aoc.Day3Pt2 do
-  # 234234234234278 would return 434234234278.
-  # We just need to find every highest number from index 0 to length-n, where n is the length left that we require.
-  # Sounds like a job for Dr Recursion.
+  @voltages 12
+
   def solve(input) do
     input
-    |> Stream.map(fn bank -> bank |> to_number_list() |> largest_joltage(0, 0) end)
+    |> Stream.map(fn bank -> bank |> to_number_list() |> find_max({0, []}, @voltages) end)
     |> Enum.sum()
   end
 
-  defp largest_joltage(numbers, left, right) do
-    case numbers do
-      [] ->
-        left * 10 + right
+  defp find_max(_, _, 0), do: 0
 
-      [last] ->
-        largest_joltage([], left, max(last, right))
+  defp find_max(numbers, {curr_max, from_curr_max}, buffer_needed)
+       when length(numbers) < buffer_needed do
+    unit = Integer.pow(10, buffer_needed - 1)
+    curr_max * unit + find_max(from_curr_max, {0, []}, buffer_needed - 1)
+  end
 
-      [n | rest] when n > left ->
-        largest_joltage(rest, n, hd(rest))
-
-      [n | rest] ->
-        largest_joltage(rest, left, max(n, right))
-    end
+  defp find_max([n | rest], {curr_max, _} = acc, buffer_needed) do
+    if n > curr_max,
+      do: find_max(rest, {n, rest}, buffer_needed),
+      else: find_max(rest, acc, buffer_needed)
   end
 
   defp to_number_list(battery_bank) do
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -1,10 +1,14 @@
 defmodule Aoc.Day3Test do
   use ExUnit.Case
 
-  test "test" do
-    assert Aoc.Day3.solve("987654321111111") == 98
-    assert Aoc.Day3.solve("811111111111119") == 89
-    assert Aoc.Day3.solve("234234234234278") == 78
-    assert Aoc.Day3.solve("818181911112111") == 92
+  test "day_3_tests" do
+    assert Aoc.Day3.solve(["987654321111111"]) == 98
+    assert Aoc.Day3.solve(["811111111111119"]) == 89
+    assert Aoc.Day3.solve(["234234234234278"]) == 78
+    assert Aoc.Day3.solve(["818181911112111"]) == 92
+  end
+
+  test "day_3_pt2_tests" do
+    assert Aoc.Day3Pt2.solve(["234234234234278"]) == 434_234_234_278
   end
 end
