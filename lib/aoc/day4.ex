7123ddc5aa545571c955ea4f0570878e65dade49
James Potter <jamesrpotter1@gmail.com>
2025-12-06 16:39:04 +0000

    day 4 pt 2

--- lib/aoc/day4.ex
+++ lib/aoc/day4.ex
@@ -5,38 +5,61 @@ defmodule Aoc.Day4 do
       |> Stream.map(&String.trim/1)
       |> Stream.map(&String.graphemes/1)
 
-    map =
+    set =
       parsed
       |> Enum.with_index()
-      |> Enum.reduce(%{}, &neighbour_adder/2)
-
-    parsed
-    |> Enum.with_index()
-    |> Enum.sum_by(fn {line, y} ->
-      line
-      |> Enum.with_index()
-      |> Enum.count(fn {ch, x} ->
-        ch == "@" and Map.get(map, {x, y}, 0) < 4
-      end)
-    end)
-  end
-
-  defp neighbour_adder({line, y}, acc) do
-    line
-    |> Enum.with_index()
-    |> Enum.reduce(acc, fn {ch, x}, acc ->
-      if ch == "@" do
-        neighbour_indices(x, y)
-        |> Enum.reduce(acc, fn {neigh_x, neigh_y}, acc ->
-          Map.update(acc, {neigh_x, neigh_y}, 1, &(&1 + 1))
+      |> Enum.reduce(MapSet.new(), fn {line, y}, acc ->
+        line
+        |> Enum.with_index()
+        |> Enum.reduce(acc, fn
+          {"@", x}, acc -> MapSet.put(acc, {x, y})
+          _, acc -> acc
         end)
-      else
-        acc
-      end
+      end)
+
+    free(set) |> length()
+  end
+
+  def solve_pt2(input) do
+    parsed =
+      input
+      |> Stream.map(&String.trim/1)
+      |> Stream.map(&String.graphemes/1)
+
+    set =
+      parsed
+      |> Enum.with_index()
+      |> Enum.reduce(MapSet.new(), fn {line, y}, acc ->
+        line
+        |> Enum.with_index()
+        |> Enum.reduce(acc, fn
+          {"@", x}, acc -> MapSet.put(acc, {x, y})
+          _, acc -> acc
+        end)
+      end)
+
+    recursively_free(set)
+  end
+
+  defp free(set) do
+    set
+    |> Enum.filter(fn coords ->
+      neighbour_indices(coords) |> Enum.count(&MapSet.member?(set, &1)) < 4
     end)
   end
 
-  defp neighbour_indices(x, y) do
+  defp recursively_free(set) do
+    now_free = free(set)
+
+    if Enum.empty?(now_free) do
+      0
+    else
+      new_set = MapSet.difference(set, MapSet.new(now_free))
+      Enum.count(now_free) + recursively_free(new_set)
+    end
+  end
+
+  defp neighbour_indices({x, y}) do
     (x - 1)..(x + 1)
     |> Enum.flat_map(fn i ->
       (y - 1)..(y + 1)
