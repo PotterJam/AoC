bf3975dbe2f7f7e8612ac76233d9a7e7c512efbe
James Potter <jamesrpotter1@gmail.com>
2025-12-06 13:10:03 +0000

    Red: part 2

--- test/day4_test.exs
+++ test/day4_test.exs
@@ -12,4 +12,16 @@ defmodule Aoc.Day4Test do
 
     assert Aoc.Day4.solve(input) == 3
   end
+
+  test "day_4_part_2" do
+    input = [
+      "@....",
+      "...@.",
+      "..@@.",
+      "..@@.",
+      "....."
+    ]
+
+    assert Aoc.Day4.solve(input) == 6
+  end
 end
