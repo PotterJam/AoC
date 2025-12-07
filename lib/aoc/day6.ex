75685e3a3321899bb1bce5a433d5433ebbf4d594
James Potter <jamesrpotter1@gmail.com>
2025-12-07 11:03:13 +0000

    New up day 6

---
+++ lib/aoc/day6.ex
@@ -0,0 +1,7 @@
+defmodule Aoc.Day6 do
+  def solve_pt1(input) do
+  end
+
+  def solve_pt2(input) do
+  end
+end
---
+++ test/day6_test.exs
@@ -0,0 +1,9 @@
+defmodule Aoc.Day6Test do
+  use ExUnit.Case
+
+  test "day_6_part_1" do
+  end
+
+  test "day_6_part_2" do
+  end
+end
