a4f5cbbb5eb25b47ed7f851dc64f8142b74485e1
James Potter <jamesrpotter1@gmail.com>
2025-12-07 16:29:22 +0000

    Red: day 6

--- test/day6_test.exs
+++ test/day6_test.exs
@@ -2,6 +2,16 @@ defmodule Aoc.Day6Test do
   use ExUnit.Case
 
   test "day_6_part_1" do
+    input =
+      """
+      123 328  51 64
+       45 64  387 23
+        6 98  215 314
+      *   +   *   +
+      """
+      |> String.split("\n")
+
+    assert Aoc.Day6.solve_pt1(input) == 4_277_556
   end
 
   test "day_6_part_2" do
