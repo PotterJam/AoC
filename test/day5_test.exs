4d79d1fbde1b47326bc2ab0feac2f5e464a560f0
James Potter <jamesrpotter1@gmail.com>
2025-12-07 10:17:53 +0000

    Red: day 5 pt2

--- test/day5_test.exs
+++ test/day5_test.exs
@@ -22,5 +22,15 @@ defmodule Aoc.Day5Test do
   end
 
   test "day_5_part_2" do
+    input =
+      """
+      3-5
+      10-14
+      12-18
+      16-17
+      """
+      |> String.split("\n")
+
+    assert Aoc.Day5.solve_pt2(input) == 12
   end
 end
