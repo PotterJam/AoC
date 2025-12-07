a68f1d7152327aaafeb3d38d9301c271e5257e23
James Potter <jamesrpotter1@gmail.com>
2025-12-07 22:29:47 +0000

    Can't quite get day 6 pt2 yet

--- lib/aoc/day6.ex
+++ lib/aoc/day6.ex
@@ -1,4 +1,6 @@
 defmodule Aoc.Day6 do
+  alias List.Chars
+
   def solve_pt1(input) do
     parsed =
       input
@@ -18,6 +20,40 @@ defmodule Aoc.Day6 do
     end)
   end
 
-  def solve_pt2(_input) do
+  def solve_pt2(input) do
+    parsed =
+      input
+      |> Stream.take_while(&(&1 != ""))
+      |> Stream.map(&String.graphemes/1)
+
+    Enum.zip_with(parsed, &Function.identity/1)
+    |> Stream.chunk_every(3, 4)
+    |> Stream.map(fn col ->
+      op = Enum.map(col, &Enum.at(&1, -1)) |> Enum.join() |> String.trim()
+      {op, col}
+    end)
+    |> Stream.filter(fn {op, _} -> op in ["+", "*"] end)
+    |> Stream.map(fn {op, col} ->
+      clean_cols = col
+
+      for n <- col, m <- n do
+        case(Integer.parse(m)) do
+          {int, ""} -> int
+          _ -> nil
+        end
+      end
+      |> Enum.chunk_every(4)
+      |> Enum.map(fn col -> Enum.filter(col, &(&1 != nil)) end)
+      |> Enum.map(&Integer.undigits/1)
+      # need a reverse somewhere?
+      |> then(fn nums ->
+        case op do
+          "+" -> Enum.sum(nums)
+          "*" -> Enum.product(nums)
+        end
+      end)
+    end)
+    |> Enum.sum()
+    |> IO.inspect()
   end
 end
--- test/day6_test.exs
+++ test/day6_test.exs
@@ -15,5 +15,29 @@ defmodule Aoc.Day6Test do
   end
 
   test "day_6_part_2" do
+    input =
+      """
+      123 328  51 64
+       45 64  387 23
+        6 98  215 314
+      *   +   *   +
+      """
+      |> String.split("\n")
+
+    assert Aoc.Day6.solve_pt2(input) == 3_263_827
+  end
+
+  test "handles empty columns" do
+    input =
+      """
+      123 328  51 64
+      2 5 64  387 23
+        6 98  215 314
+      +   *        +
+      """
+      |> String.split("\n")
+
+    # todo
+    assert Aoc.Day6.solve_pt2(input) == 9_999_999
   end
 end
